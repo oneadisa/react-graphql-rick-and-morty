8092dc1d9b49b45c8036910aad98de1576778d41
Abdul Ibrahim <theoneadisa@gmail.com>
2022-08-12 12:51:26 +0100

    list all characters

--- src/pages/charactersList.tsx
+++ src/pages/charactersList.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+// import React from 'react'
+import {useQuery, gql} from '@apollo/client'
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from 'react'
+
+const GET_CHARACTERS = gql`
+query{
+  characters{
+    results{
+      id
+      name
+      image
+    }
+  }
+}
+`
+
 
 export const  CharacterList = () => {
 
+
+    const {error, loading, data} = useQuery(GET_CHARACTERS)
+
+    console.log({error, loading, data})
+
+    if (loading) return <div> Spinner... </div>
+
+    if(error) return <div> Something went wrong</div>
     return(
-        <>
-        </>
+        <div className="spinner">
+            {data.characters.results.map((char: { image: string | undefined; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined }) => {
+                return(
+                    <div className='character'>
+                        <img src={char.image} alt="character" />
+                        <p> {char.name}</p>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
